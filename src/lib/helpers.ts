e944d2f1bb2d70119d19740616f9ff9f1cd82089
RajputKashish <kashish@example.com>
2025-12-11 16:32:19 +0530

    feat: add helper functions

---
+++ src/lib/helpers.ts
@@ -0,0 +1,22 @@
+export const MainRoutes = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Demo Videos",
+    href: "/demo",
+  },
+  {
+    label: "About Us",
+    href: "/about",
+  },
+  {
+    label: "Services",
+    href: "/services",
+  },
+  {
+    label: "Contact Us",
+    href: "/contact",
+  },
+];
